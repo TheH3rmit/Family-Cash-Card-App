b498c793c538d42d106243abbc829fb729276ce8
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2024-04-24 22:06:31 +0200

    Simple unit tests for de/serialisation json files

---
+++ src/main/java/com/example/familycashcardapp/CashCard.java
@@ -0,0 +1,5 @@
+package com.example.familycashcardapp;
+
+record CashCard(Long id, Double amount) {
+}
+
--- src/test/java/com/example/familycashcardapp/FamilyCashCardAppApplicationTests.java
+++ src/test/java/com/example/familycashcardapp/FamilyCashCardAppApplicationTests.java
@@ -1,13 +1,21 @@
 package com.example.familycashcardapp;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class FamilyCashCardAppApplicationTests {
 
+
     @Test
-    void contextLoads() {
+    void Test() {
+        assertThat(42).isEqualTo(42);
     }
 
 }
