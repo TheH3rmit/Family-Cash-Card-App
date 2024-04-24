2c591447f0b68e7eff49f7f00bed302212814da0
TheH3rmit <95000621+TheH3rmit@users.noreply.github.com>
2024-04-24 18:37:11 +0200

    Initial

---
+++ src/test/java/com/example/familycashcardapp/FamilyCashCardAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.familycashcardapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
